6be2c68e02848d6fdde882cdae9b0f1f32320add
shiqihuana <shiqihuana@icloud.com>
2026-08-26 16:15:21 +0800

    线性回归模型的训练1

--- deeplearning/linear_regression.py
+++ deeplearning/linear_regression.py
@@ -4,12 +4,66 @@
 import random
 import torch
 from d2l import torch as d2l
-def synthetic_data(w,b,num_exaples):    # 生成y=Xw+b+噪声。
-    X=torch.normal(0,1(num_exalpes,len(w)))
-    y=torch.matmul(x,w)+b
+# 人工数据集
+def synthetic_data(w,b,num_examples):    # 生成y=Xw+b+噪声。
+    X=torch.normal(0,1,(num_examples,len(w)))
+    y=torch.matmul(X,w)+b
     y+=torch.normal(0,0.01,y.shape)
     return X,y.reshape((-1,1))
 true_w=torch.tensor([2,-3.4])
 true_b=4.2
 features,labels=synthetic_data(true_w,true_b,1000)
 
+print('features:',features[0],'\nlabel:',labels[0])
+d2l.set_figsize()
+d2l.plt.scatter(features[:,1].detach().numpy(),labels.detach().numpy(),1);
+d2l.plt.show()
+#
+#生成小批量数据
+def data_iter(batch_size,features,labels):
+    num_examples=len(features)
+    indices=list(range(num_examples))       # 样本是随机读取的，没有特定顺序
+    random.shuffle(indices)
+    for i in range(0,num_examples,batch_size):
+        batch_indices=torch.tensor(indices[i:min(i+batch_size,num_examples)])
+        yield features[batch_indices],labels[batch_indices]
+batch_size=10
+for X,y in data_iter(batch_size,features,labels):
+    print(X,'\n',y)
+    break
+# 
+# 初始化模型参数
+w = torch.normal(0,0.01,size=(2,1),requires_grad=True)
+b = torch.zeros(1,requires_grad = True)
+# 
+# 定义模型
+def linreg (X,w,b):
+    return torch.matmul(X,w)+b
+# 
+# 定义损失函数
+def squared_loss(y_hat,y):
+    # 均方损失
+    return (y_hat - y.reshape(y_hat.shape))**2/2
+# 
+# 定义优化算法
+def sqd(params,lr,batch_size):
+    # 小批量随机梯度下降
+    with torch.no_grad():
+        for param in params:
+            param-= lr * param.grad / batch_size
+            param.grad.zero_()
+#**********************
+#训练过程
+lr= 0.03        # 学习率：梯度下降的步长
+num_epoch = 3   # 迭代次数
+net = linreg    # 模型选择
+loss = squared_loss # 损失函数选择
+
+for epoch in range(num_epochs):
+    for X,y in data_iter(batch_size,features,labels):
+        l= loss(net(X,w,b),y)
+        l.sum().backward()
+        sgd([w,b],lr,batch_size)
+    with torch.no_grad():
+        train_l = loss(net(features,w,b),labels)
+        print(f'epoch{epoch + 1},loss{float(train_l.mean()):f}')
